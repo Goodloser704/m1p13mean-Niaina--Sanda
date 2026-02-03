f16dd45d96be12698c642630d7635726544954f2
SandaRaz <25sandanirina@gmail.com>
2026-02-03 11:39:27 +0300

    default files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeTest } from './pages/home-test/home-test';
+import { AuthLayout } from './shared/layout/auth-layout/auth-layout';
 
 export const routes: Routes = [
+  {
+    path: '', 
+    component: AuthLayout,
+    children: []
+  },
   { path: 'home', component: HomeTest },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
---
+++ frontend/src/app/shared/layout/commercant-layout/commercant-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commercant-layout',
+  imports: [],
+  templateUrl: './commercant-layout.html',
+  styleUrl: './commercant-layout.scss',
+})
+export class CommercantLayout {
+
+}
