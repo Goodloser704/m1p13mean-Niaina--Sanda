2318f6e010b091b9a1bb18b9d77484a6ca206e14
SandaRaz <25sandanirina@gmail.com>
2026-03-02 23:38:27 +0300

    achat-color-state

--- frontend/src/app/components/commercant/achat-card/achat-card.ts
+++ frontend/src/app/components/commercant/achat-card/achat-card.ts
@@ -11,6 +11,7 @@ import { DatePipe, CurrencyPipe, NgClass } from "@angular/common";
 export class AchatCard implements OnInit {
   @Input() achat!: Achat;
   @Input() showValidateButton = false;
+  @Input() commercant = false;
 
   @Output() validate = new EventEmitter<string>();
 
@@ -47,13 +48,56 @@ export class AchatCard implements OnInit {
     }
 
     // Plus d'1 heure → afficher date complète
-    return new Date(this.achat.typeAchat.dateFin)
+    const dayPart = new Date(this.achat.typeAchat.dateFin)
       .toLocaleString('fr-FR', {
         day: '2-digit',
         month: '2-digit',
-        year: 'numeric',
+        year: 'numeric'
+      });
+
+    const hoursPart = new Date(this.achat.typeAchat.dateFin)
+      .toLocaleString('fr-FR', {
         hour: '2-digit',
         minute: '2-digit'
       });
+    return `Disponible le ${dayPart} à ${hoursPart}`;
+  }
+
+  getLivraisonLabel(): string {
+    if (!this.achat?.typeAchat?.dateFin) return '';
+
+    const dateFin = new Date(this.achat.typeAchat.dateFin);
+    let typeAchatAction = '';
+
+    if (this.achat.typeAchat.type === TypeAchat.Livrer) {
+      typeAchatAction = 'Livré le';
+    } else if (this.achat.typeAchat.type === TypeAchat.Recuperer) {
+      typeAchatAction = 'Récupérable le';
+    }
+
+    const dayPart = dateFin.toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    const hoursPart = dateFin.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
+
+    return `${typeAchatAction} ${dayPart} à ${hoursPart}`;
+  }
+
+  getLivraisonClass(): string {
+    if (!this.achat?.typeAchat?.dateFin) return 'text-muted';
+
+    const dateFin = new Date(this.achat.typeAchat.dateFin);
+    const now = new Date();
+
+    // Calculer dateFin + 15 minutes
+    const dateFinPlus15 = new Date(dateFin.getTime() + 15 * 60 * 1000);
+
+    if (now < dateFin) {
+      return 'text-info'; // pas encore disponible
+    } else if (now >= dateFin && now <= dateFinPlus15) {
+      return 'text-success'; // disponible dans la fenêtre 15 min
+    } else if (now > dateFinPlus15) {
+      return 'text-danger'; // dépassé
+    }
+
+    return 'text-muted'; // fallback
   }
 }
